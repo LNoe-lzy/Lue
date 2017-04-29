1c575a12adb258f1ec5725dbcb3861a67c4b02b6
LNoe-lzy <m15222505516@163.com>
2017-04-29 08:43:40 +0800

    增加事件传递

--- src/main.js
+++ src/main.js
@@ -11,7 +11,8 @@ class Event {
     off(e) {
         for (let key in this.events) {
             if (this.events.hasOwnProperty(key) && key === e) {
-                delete this.events[e];e
+                delete this.events[e];
+                e
             }
         }
     }
@@ -30,14 +31,18 @@ class Observer {
         this.eventHub = new Event();
         this._walk(data);
     }
-
     _walk(data) {
         let val;
         for (let key in data) {
             if (data.hasOwnProperty(key)) {
                 val = data[key];
                 if (typeof val === 'object') {
-                    new Observer(val);
+                    // 如果是对象，则递归创建新的observer对象，并保存parent对象
+                    let obj = new Observer(val);
+                    obj.parent = {
+                        key,
+                        obj: this
+                    };
                 }
                 this._convert(key, val);
             }
@@ -51,19 +56,32 @@ class Observer {
             configurable: true,
             get() {
                 return val;
-            }, 
+            },
             set(newval) {
                 console.log(`你设置了新值: ${newval}`);
                 if (val === newval) {
                     return;
                 }
-                that.eventHub.emit(key, val, newval);
+                that._notify(key, val, newval)
                 val = newval;
                 if (typeof newval === 'object') {
                     new Observer(val);
                 }
             }
-        })
+        });
+    }
+
+    _notify(e, path, val) {
+        // 执行事件
+        this.eventHub.emit(e, path, val);
+        // 事件传播
+        let parent = this.parent;
+        if (!parent) {
+            return;
+        }
+        // 递归传播
+        let obj = parent.obj;
+        obj._notify(e, path, val);
     }
 
     watch(e, handler) {
